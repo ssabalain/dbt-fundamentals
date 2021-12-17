733c968a92aabb0d040a4f737046036267adfcc5
Santiago Sabalain <santiago.sabalain@bairesdev.com>
2021-12-17 12:27:00 +0000

    Testing & documentation added

---
+++ tests/assert_stg_payments_total_value_is_positive.sql
@@ -0,0 +1,13 @@
+with payments as (
+    SELECT * FROM {{ ref('stg_payments') }}
+)
+
+SELECT
+    order_id,
+    SUM(amount) as total_amount
+FROM
+    payments
+GROUP BY
+    order_id
+HAVING 
+    total_amount < 0
